f1fa7acb894ea34593b360ec8f5b7b3e4d9c7827
Harsh Raundal <dkingston0000@gmail.com>
2025-06-08 21:56:00 +0530

    Create histogram practice.py

---
+++ histogram practice.py
@@ -0,0 +1,14 @@
+#histogram python code
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+ages = [2,5,70,40,30,45,50,45,43,40,44,60,7,13,57,18,90,77,32,21,20,40,45,32,38]
+range = (0, 100)
+bins = 5
+plt.hist(ages,bins,range,color ='blue',
+         histtype ='bar', rwidth = 0.8)
+plt.xlabel('age')
+plt.ylabel('No. of people')
+plt.title('histogram plot')
+plt.show()
